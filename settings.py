aab3ee8a536550942c2c1f1008dc8ff3ac7cb06e
Sadra Sabouri <sabouri.sadra@gmail.com>
2025-05-17 17:36:11 -0700

    add : setting files added.

---
+++ settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Settings module."""
+
+NAME = "🐍 Snake Game"
+BACKGROUND_COLOR = "black"
+SNAKE_COLOR = "green"
+SNAKE_SHAPE = "square"
+FOOD_COLOR = "red"
+FOOD_SHAPE = "circle"
+WIDTH, HEIGHT = 600, 600
+
+SCORE_DISPLAY_COLOR = "white"
+SCORE_DISPLAY_FONT = ("Courier", 20, "normal")
+SCORE_DISPLAY_FORMAT = "Score: {score}  High Score: {high_score}"
+SCORE_WIDTH, SCORE_HEIGHT = 0, 260
